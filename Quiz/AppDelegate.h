2a3131888836b4289487d73870feeae19b1b9671
Yinlin Zhang <yinlinz@vmware.com>
2015-06-20 19:14:00 +0800

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Yinlin Zhang on 6/20/15.
+//  Copyright (c) 2015 Yinlin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
